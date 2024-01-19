a5ec7546bcc6648fe68f2a6ea5859bfbc0ab047b
Binaya Timsina <btimsina3@gatech.edu>
2024-01-18 21:04:33 -0500

    Added Backend Search method

--- src/main/java/iluvus/backend/api/controller/CommunityController.java
+++ src/main/java/iluvus/backend/api/controller/CommunityController.java
@@ -1,6 +1,8 @@
 package iluvus.backend.api.controller;
 
 import iluvus.backend.api.service.CommunityService;
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/community")
@@ -36,5 +41,14 @@ public class CommunityController {
     @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ArrayList<String>> getAllCommunity() {
         return ResponseEntity.ok().body(communityService.getAllCommunity());
+    }
+
+    @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ArrayList<String>> searchCommunity(@RequestParam String filter) {
+        ArrayList<String> communityList = communityService.getAllCommunity();
+        ArrayList<String> filteredCommunityList = communityList.stream()
+            .filter(community -> community.contains(filter))
+            .collect(Collectors.toCollection(ArrayList::new));
+        return ResponseEntity.ok().body(filteredCommunityList);
     }
 }
