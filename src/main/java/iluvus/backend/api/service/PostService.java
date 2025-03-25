8f6eb744dff84c94f1cebfc39bb48c1982203725
shreya-devaraju <shreyadevaraju@yahoo.com>
2025-03-24 22:07:56 -0400

    Imports for Post Service Pagenation

--- src/main/java/iluvus/backend/api/service/PostService.java
+++ src/main/java/iluvus/backend/api/service/PostService.java
@@ -14,13 +14,16 @@ import iluvus.backend.api.repository.UserRepository;
 
 import java.util.*;
 
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-// New Imports...
-import java.util.stream.Collectors;
 
 // create post service class
 @Service
