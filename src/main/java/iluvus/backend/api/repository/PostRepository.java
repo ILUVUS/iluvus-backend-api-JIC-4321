d96984c0e02f3de15b0f94cda631f730e8e82082
shreya-devaraju <shreyadevaraju@yahoo.com>
2025-03-24 22:05:57 -0400

    new post repo queries

--- src/main/java/iluvus/backend/api/repository/PostRepository.java
+++ src/main/java/iluvus/backend/api/repository/PostRepository.java
@@ -1,6 +1,10 @@
 package iluvus.backend.api.repository;
 
+import iluvus.backend.api.model.InterestTopic;
 import iluvus.backend.api.model.Post;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,11 +12,17 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    //make sure these queries are not too expensive
     @Query("{'author_id': ?0}")
     List<Post> findPostByAuthor_id(String author_id);
 
     @Query("{'community_id': ?0}")
     List<Post> findPostByCommunity_id(String community_id);
+
+    @Query("{ 'community_id': { $in: ?0 } }")
+    Page<Post> findPostsByCommunityandTopicIds(List<String> communityIds, List<Integer> topicIds, Pageable pageable);
+    //added new import pageable to return page of posts instead of full list
 
     @Query("{ 'sharedBy': { $in: [?0] } }")
     List<Post> findPostsSharedByUser(String userId);
