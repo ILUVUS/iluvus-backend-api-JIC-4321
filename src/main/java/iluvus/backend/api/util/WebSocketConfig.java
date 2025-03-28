e1df80d90cf4574f209f13c514ceb8573627af27
shreya-devaraju <shreyadevaraju@yahoo.com>
2025-03-28 13:45:20 -0400

    New WebsocketConfig

---
+++ src/main/java/iluvus/backend/api/util/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package iluvus.backend.api.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final MyWebSocketHandler myWebSocketHandler;
+
+    public WebSocketConfig(MyWebSocketHandler myWebSocketHandler) {
+        this.myWebSocketHandler = myWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myWebSocketHandler, "/ws").setAllowedOrigins("*");
+    }
+}
+
