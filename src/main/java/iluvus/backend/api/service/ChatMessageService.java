65e0f3fbea28d1f7dcb7fca5ab42bd53ee355409
shreya-devaraju <shreyadevaraju@yahoo.com>
2025-03-28 10:24:53 -0400

    Basic checks for sending messages

--- src/main/java/iluvus/backend/api/service/ChatMessageService.java
+++ src/main/java/iluvus/backend/api/service/ChatMessageService.java
@@ -1,5 +1,80 @@
 package iluvus.backend.api.service;
 
+import iluvus.backend.api.dto.ChatMessageDto;
+import iluvus.backend.api.model.ChatMessage;
+import iluvus.backend.api.repository.ChatMessageRepository;
+import iluvus.backend.api.repository.UserRepository;
+import iluvus.backend.api.repository.ChatRoomRepository;
+import iluvus.backend.api.model.ChatRoom;
+import iluvus.backend.api.resources.NotificationType;
+import iluvus.backend.api.model.User;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+// New Imports...
+import java.util.stream.Collectors;
+
+
+@Service
 public class ChatMessageService {
+
+    @Autowired
+    private ChatMessageRepository chatMessageRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
+    public List<ChatMessage> sendMessage(Map<String, String> data) {
+        try {
+            String roomId = data.get("roomId");
+            String senderId = data.get("senderId");
+            String message = data.get("message");
+            String timestamp = data.get("timestamp");
+
+            if (message == null) {
+                return null;
+            }
+
+            if (roomId == null) {
+                return null;
+            }
+
+            if (senderId == null) {
+                return null;
+            }
+
+            if (timestamp == null) {
+                return null;
+            }
+
+            //this will most likely be a bottleneck need to restructure later
+            if (!userRepository.existsById(senderId)) {
+                return null;
+            }
+
+            if (!chatRoomRepository.existsById(roomId)) {
+                return null;
+            } 
+
+
+
+
+
+
+
+
+        } catch (Exeception e) {
+
+        }
+    }
+
+
     
 }
--- src/main/java/iluvus/backend/api/service/ChatRoomService.java
+++ src/main/java/iluvus/backend/api/service/ChatRoomService.java
@@ -1,5 +1,22 @@
 package iluvus.backend.api.service;
 
+import iluvus.backend.api.dto.ChatRoomDto;
+import iluvus.backend.api.dto.UserDto;
+import iluvus.backend.api.model.User;
+import iluvus.backend.api.repository.ChatRoomRepository;
+import iluvus.backend.api.repository.UserRepository;
+import iluvus.backend.api.resources.NotificationType;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
 public class ChatRoomService {
     
 }
