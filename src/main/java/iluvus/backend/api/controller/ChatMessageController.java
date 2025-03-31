a82e1722519bfeeac68ecaf0fe84024261063c5f
Christeena <christeenamjoby@gmail.com>
2025-03-31 11:50:02 -0400

    'sd'

--- src/main/java/iluvus/backend/api/controller/ChatMessageController.java
+++ src/main/java/iluvus/backend/api/controller/ChatMessageController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/chat_messsage")
+@RequestMapping("/chat_message")
 public class ChatMessageController {
 
     @Autowired
