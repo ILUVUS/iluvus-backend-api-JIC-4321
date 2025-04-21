8f00c84ec57c21383bf3ec5f4a9b6fb81acf8f1a
shreya-devaraju <shreyadevaraju@yahoo.com>
2025-04-21 00:16:15 -0400

    File Upload Config

--- src/main/java/iluvus/backend/api/config/FileUploadConfig.java
+++ src/main/java/iluvus/backend/api/config/FileUploadConfig.java
@@ -1,5 +1,20 @@
 package iluvus.backend.api.config;
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import jakarta.servlet.MultipartConfigElement;
+
+@Configuration
 public class FileUploadConfig {
-    
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(25));        
+        factory.setMaxRequestSize(DataSize.ofMegabytes(25));    
+        return factory.createMultipartConfig();
+    }
 }
